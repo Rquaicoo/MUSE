f918446f9a0745a26d0d12ef647cbde508feaf51
Russell Quaicoo <russellquaicoo1@gmail.com>
2021-12-26 18:45:43 +0000

    added music sumarry page called mymusic.js

--- musicapp/musef/screens/Tabs.js
+++ musicapp/musef/screens/Tabs.js
@@ -2,8 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 
 import Home from './Home';
-import Intro3 from './intro3';
-import Intro2 from './intro2';
+import MyMusic from './mymusic';
 import { MaterialIcons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
@@ -15,33 +14,35 @@ const Tab = createBottomTabNavigator();
 function Tabs() {
   return (
     <Tab.Navigator 
-      screenOptions={{
+    
+    screenOptions={{
         activeTintColor: '#ffc107',
         inactiveTintColor: '#bdbdbd',
         showLabel: false,
-          style: {
-              backgroundColor: '#494a47',
-              height:'20%',
+          tabBarStyle: {
+              backgroundColor: '#151723',
+              
           },
-      }}>
+      }}
+     >
       <Tab.Screen name="Browser" component={Home} 
       options={{ tabBarLabel:('Home'), headerShown: false , tabBarIcon: ({color}) => 
-      <MaterialIcons name="library-music" size={24} color="black" /> }} />
+      <MaterialIcons name="library-music" size={24} color="white" /> }} />
       <Tab.Screen name="Artists" component={Home} 
       options={{ tabBarLabel:('Artists'), headerShown: false , tabBarIcon: ({color}) => 
-      <MaterialCommunityIcons name="account-music-outline" size={24} color="black" /> }}/>
+      <MaterialCommunityIcons name="account-music-outline" size={24} color="white" /> }}/>
       <Tab.Screen name="Home" component={Home} 
       options={{ tabBarLabel:() => {return null}, headerShown: false , tabBarIcon: ({color}) => 
-      <View>
-      <MaterialCommunityIcons name="home-circle" size={54} color="black" />
+      <View style={{backgroundColor: "#151723"}}>
+      <MaterialCommunityIcons name="home-circle" size={60} color="white" />
       </View>
     }}/>
       <Tab.Screen name="Genres" component={Home} 
       options={{ tabBarLabel:('Genres'), headerShown: false , tabBarIcon: ({color}) => 
-      <Feather name="music" size={24} color="black" /> }}/>
-      <Tab.Screen name="My Music" component={Home} 
+      <Feather name="music" size={24} color="white" /> }}/>
+      <Tab.Screen name="My Music" component={MyMusic} 
       options={{ tabBarLabel:('My Music'), headerShown: false , tabBarIcon: ({color}) => 
-      <Ionicons name="headset-outline" size={24} color="black" /> }} />
+      <Ionicons name="headset-outline" size={24} color="white" /> }} />
     </Tab.Navigator>
   );
 }
