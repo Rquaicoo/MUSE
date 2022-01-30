f49f6509d8abd688f5c1a8dac61fe003c26f376b
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-01-30 22:32:10 +0000

    addded whitenoise to settings

--- musicapp/musicapp/musicapp/settings.py
+++ musicapp/musicapp/musicapp/settings.py
@@ -72,6 +72,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -156,11 +157,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
+'''STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-]
+]'''
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
