d0e0a68e69b5aef17c67a7e86f821e87de829fb1
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-02-15 14:58:44 +0000

    added new music file for local songs

--- musicapp/musicapp/musicapp/settings.py
+++ musicapp/musicapp/musicapp/settings.py
@@ -157,9 +157,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-'''STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-]'''
+]
 
 MEDIA_URL = '/images/'
 
