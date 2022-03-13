433f5d209b0b9b934d8ea1f1628d9455f84b0206
Russell Quaicoo <russellquaicoo1@gmail.com>
2022-03-13 12:28:28 +0000

    timer for moving slider added

--- musicapp/musef/screens/LocalAudio.js
+++ musicapp/musef/screens/LocalAudio.js
@@ -4,15 +4,16 @@ import { Feather, AntDesign,Entypo, Ionicons,FontAwesome5, SimpleLineIcons,FontA
 import { AudioContext } from './AudioProvider';
 import { useNavigation } from '@react-navigation/native';
 
-const toMinutesAndSeconds = (secs) => {
-    var minutes = Math.floor(secs / 60);
-    var seconds = ((secs % 60) / 1).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
-  }
+
 
 export class LocalAudio extends React.Component {
     static contextType = AudioContext;
     
+    toMinutesAndSeconds = (secs) => {
+        var minutes = Math.floor(secs / 60);
+        var seconds = ((secs % 60) / 1).toFixed(0);
+        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+      }
     
     render() {
         const { navigation } = this.props;
@@ -45,7 +46,7 @@ export class LocalAudio extends React.Component {
                             <Text style={{fontSize:16,color:'#fff', fontWeight:'bold',}}>{item.filename}</Text>
                             <View style={{display: "flex", flexDirection: "row",}} >
                             </View>
-                            <Text style={{fontSize:12,color:'#fff', fontWeight:'bold',}}>{toMinutesAndSeconds(item.duration)}</Text>
+                            <Text style={{fontSize:12,color:'#fff', fontWeight:'bold',}}>{this.toMinutesAndSeconds(item.duration)}</Text>
                         </View>
                         <View style={{justifyContent: "center", flex:1}}>
                             <Feather name="play" size={24} color="white" />
