557b005df607cb06cfad44b6b5c37b2fbf13c913
Rquaicoo <russellquaicoo1@gmail.com>
2022-10-12 18:01:28 +0000

    modify website homepage

--- musicapp/server/musebpublic/url.py
+++ musicapp/server/musebpublic/url.py
@@ -2,7 +2,11 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.signUp, name='signUp'),
+    path('', views.home, name='signUp'),
+
+    path('join_waiting_list/', views.signUp, name='signUp'),
+
     path('users/', views.users, name='users'),
+    
     path('download_apk/', views.downloadApp, name='downloadApp'),
 ]
